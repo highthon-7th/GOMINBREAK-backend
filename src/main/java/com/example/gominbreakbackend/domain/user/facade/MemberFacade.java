a6ecff972d5d56cf8d9a32a77ef0aec1bdf0bb04
이정윤 <dlwjddbs14@naver.com>
2022-01-15 18:58:21 +0900

    ⚡ :: member facade settings

--- src/main/java/com/example/gominbreakbackend/domain/user/facade/MemberFacade.java
+++ src/main/java/com/example/gominbreakbackend/domain/user/facade/MemberFacade.java
@@ -1,2 +1,27 @@
-package com.example.gominbreakbackend.domain.user.facade;public class MemberFacade {
+package com.example.gominbreakbackend.domain.user.facade;
+
+import com.example.gominbreakbackend.domain.user.domain.Member;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class MemberFacade {
+
+    public static Member getMember(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || authentication.getPrincipal() == null
+                || !(authentication.getPrincipal() instanceof Member))
+            return null;
+
+        return (Member) authentication.getPrincipal();
+    }
+
+    public static Integer getMemberId() {
+        if (getMember() == null) {
+            return null;
+        }
+
+        return getMember().getId();
+    }
 }
+
