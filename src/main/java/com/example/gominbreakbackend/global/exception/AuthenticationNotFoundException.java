539172a8bcf022716e11fbd3af2adbc6ee9fda55
이정윤 <dlwjddbs14@naver.com>
2022-01-15 23:40:27 +0900

    ⚡ :: exception settings

--- src/main/java/com/example/gominbreakbackend/global/exception/AuthenticationNotFoundException.java
+++ src/main/java/com/example/gominbreakbackend/global/exception/AuthenticationNotFoundException.java
@@ -1,2 +1,14 @@
-package com.example.gominbreakbackend.global.exception;public class AuthenticationNotFoundException {
+package com.example.gominbreakbackend.global.exception;
+
+import com.example.gominbreakbackend.global.error.ErrorCode;
+import com.example.gominbreakbackend.global.error.exception.GominException;
+
+public class AuthenticationNotFoundException extends GominException {
+
+    public static GominException EXCEPTION =
+            new AuthenticationNotFoundException();
+
+    private AuthenticationNotFoundException(){
+        super(ErrorCode.AUTHENTICATION_NOT_FOUND);
+    }
 }
