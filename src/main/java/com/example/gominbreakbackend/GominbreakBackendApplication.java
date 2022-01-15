7bb249d2ead54569a770044fc069a76f2795b726
이정윤 <dlwjddbs14@naver.com>
2022-01-15 16:07:40 +0900

    ⚙ :: project settings

---
+++ src/main/java/com/example/gominbreakbackend/GominbreakBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.gominbreakbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GominbreakBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GominbreakBackendApplication.class, args);
+    }
+
+}
