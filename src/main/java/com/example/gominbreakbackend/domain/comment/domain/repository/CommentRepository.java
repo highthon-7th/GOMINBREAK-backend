98dfbd068a63cc487f4ca985a06d934e6984a4cf
이정윤 <dlwjddbs14@naver.com>
2022-01-15 23:38:14 +0900

    ⚡ :: comment count settings

--- src/main/java/com/example/gominbreakbackend/domain/comment/domain/repository/CommentRepository.java
+++ src/main/java/com/example/gominbreakbackend/domain/comment/domain/repository/CommentRepository.java
@@ -3,5 +3,8 @@ package com.example.gominbreakbackend.domain.comment.domain.repository;
 import com.example.gominbreakbackend.domain.comment.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
+
 }
