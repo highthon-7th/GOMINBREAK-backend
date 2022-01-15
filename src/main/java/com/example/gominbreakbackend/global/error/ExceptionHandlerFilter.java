3ef9d517beb48e9701602544b49d1ad80b5f67c0
이정윤 <dlwjddbs14@naver.com>
2022-01-15 21:31:55 +0900

    🔧 :: fix class

--- src/main/java/com/example/gominbreakbackend/global/error/ExceptionHandlerFilter.java
+++ src/main/java/com/example/gominbreakbackend/global/error/ExceptionHandlerFilter.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
-public class ExceptionHandler extends OncePerRequestFilter {
+public class ExceptionHandlerFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
